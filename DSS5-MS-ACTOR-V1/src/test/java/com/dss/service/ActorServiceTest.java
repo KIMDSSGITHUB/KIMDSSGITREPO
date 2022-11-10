cbec2ebe1e00bd79f5cbc6759fd42ee956baa757
KIMDSSGITHUB <kimdssgithub@gmail.com>
2022-11-10 18:36:12 +0800

    Add Mock Rest Template, Finish findMovieByActorId test, and add deleteActorWhenActorCannotBeDeleted test.

--- DSS5-MS-ACTOR-V1/src/test/java/com/dss/service/ActorServiceTest.java
+++ DSS5-MS-ACTOR-V1/src/test/java/com/dss/service/ActorServiceTest.java
@@ -4,14 +4,15 @@ import com.dss.dto.ActorRequestDTO;
 import com.dss.dto.ActorsDTO;
 import com.dss.dto.MovieDTO;
 import com.dss.entity.Actor;
+import com.dss.exception.ActorException;
 import com.dss.exception.ActorNotFoundException;
 import com.dss.repository.ActorRepository;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
 
@@ -25,12 +26,14 @@ class ActorServiceTest {
     @Mock
     private ActorRepository actorRepository;
 
+    @Mock
+    private RestTemplate restTemplate;
+
     @InjectMocks
     private ActorService actorService = new ActorServiceImpl();
 
     private static final UUID ID = UUID.randomUUID();
     private static final UUID MOVIE_ID = UUID.randomUUID();
-    private static final MovieDTO MOVIE_RES = new MovieDTO();
     private static final ActorRequestDTO UPDATE_REQ = new ActorRequestDTO();
     private static final ActorsDTO RES = new ActorsDTO(new ArrayList<>(new ArrayList<>(Collections.singleton(new Actor()))));
     private static final List<Actor> ACTOR_LIST = new ArrayList<>();
@@ -90,13 +93,13 @@ class ActorServiceTest {
     }
 
     @Test
-    @Disabled("Work in Progress")
     @DisplayName("Find Movie By Actor Id When Actor Exists")
     void findMovieByActorId() {
-        Optional<Actor> optionalActor = Optional.of(ACTOR);
+        Optional<Actor> optionalActor = Optional.of(mockActor());
         when(actorRepository.findById(ID)).thenReturn(optionalActor);
+        when(restTemplate.getForObject("http://MS-MOVIE-SERVICE/movies/" + MOVIE_ID, MovieDTO.class)).thenReturn(mockMovie());
         MovieDTO res = actorService.getMovieByActorId(ID);
-        assertEquals(MOVIE_RES,res);
+        assertEquals(mockMovie(),res);
     }
 
 
@@ -131,4 +134,33 @@ class ActorServiceTest {
         verify(actorRepository, times(1)).deleteById(ID);
     }
 
+    @Test
+    @DisplayName("Delete Actor When Actor Cannot Be Deleted")
+    void deleteActorWhenActorCannotBeDeleted() {
+        Optional<Actor> optionalActor = Optional.of(mockActor());
+        when(actorRepository.findById(ID)).thenReturn(optionalActor);
+        ActorException exception = assertThrows(ActorException.class,
+                () -> actorService.delete(ID));
+
+        assertEquals("Actor cannot be deleted", exception.getMessage());
+    }
+
+    private Actor mockActor(){
+        Actor actor = new Actor();
+        actor.setActorId(ID);
+        actor.setFirstName("Kim");
+        actor.setLastName("Santos");
+        actor.setGender("Male");
+        actor.setMovieId(MOVIE_ID);
+        return actor;
+    }
+    private MovieDTO mockMovie() {
+        MovieDTO movie = new MovieDTO();
+        movie.setMovieId(MOVIE_ID);
+        movie.setImage("No Time to Die.jpg");
+        movie.setMovieTitle("No Time to Die");
+        movie.setCost(3100000);
+        movie.setYrOfRelease(2021);
+        return movie;
+    }
 }
